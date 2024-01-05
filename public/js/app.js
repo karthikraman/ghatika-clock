167f7fdbec1a3e5cf048ce67a4cbb96b23f51847
Karthik Raman <karthikraman@users.noreply.github.com>
2024-01-05 08:41:50 +0530

    fixed error in braahma

--- public/js/app.js
+++ public/js/app.js
@@ -136,7 +136,7 @@ function showPosition(position) {
 
   var t_rise_tmrw = get_float_h(SunCalc.getTimes(tmrw, latitude, longitude)['sunrise']);
 
-  document.getElementById('braahma').innerHTML = getKaalaTimings(t_set_yday-24, t_rise, 13, 15, 15);
+  document.getElementById('braahma').innerHTML = getKaalaTimings(t_set_yday-24, t_rise, 13, 14, 15);
   document.getElementById('prAtaH sandhyA').innerHTML = getKaalaTimings(t_set_yday-24, t_rise, 14, 15, 15).substring(0, 5) + '–' +  getKaalaTimings(t_rise, t_set, 4, 5, 15).substring(0,5);
   document.getElementById('mAdhyAhnika sandhyA').innerHTML = getKaalaTimings(t_rise, t_set, 5, 13, 15);
   document.getElementById('sAyaM sandhyA').innerHTML = getKaalaTimings(t_rise, t_set, 14, 15, 15).substring(0, 5) + '–' +  getKaalaTimings(t_set, t_rise_tmrw + 24, 1, 2, 15).substring(0, 5);
